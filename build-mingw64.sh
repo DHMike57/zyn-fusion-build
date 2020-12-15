7bebdebdc7d6e5943d4a3920f1b530d8fd21da33
DHMike57 <ml@lapsley.org.uk>
2020-12-15 20:10:08 +0000

    python2 dep appears to have disappeared.

--- build-mingw64.sh
+++ build-mingw64.sh
@@ -2,7 +2,7 @@ set -e
 echo "----------------------Getting Packages-----------------------"
 pacman 		-Syuu --noconfirm
 pacman 		-S --noconfirm --needed git ruby gcc bison util-macros automake libtool mingw-w64-x86_64-cmake cmake mingw-w64-x86_64-mruby python3 autoconf zip make
-pacman 		-S --noconfirm --needed mingw-w64-x86_64-gcc mingw-w64-x86_64-make python2 mingw-w64-x86_64-gcc-fortran mingw-w64-x86_64-gcc-libgfortran
+pacman 		-S --noconfirm --needed mingw-w64-x86_64-gcc mingw-w64-x86_64-make  mingw-w64-x86_64-gcc-fortran mingw-w64-x86_64-gcc-libgfortran
 mkdir pkg -p
 
 echo "------------------------Cleaning-----------------------------"
